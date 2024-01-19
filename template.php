4a9424e91c739e36037dd9fb13cfc71b952a8d6f
Henri Susanto <henry.dinus@gmail.com>
2024-01-19 08:53:13 +0700

    mockup

--- template.php
+++ template.php
@@ -1,35 +1,296 @@
 <div class="row-with-2-columns">
-  <table>
-    <thead>
-      <tr>
-        <th colspan="2">Mortgage Information</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Loan Amount</td>
-        <td>150,000.00</td>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <th>Monthly Payment</th>
-        <th>85.68</th>
-      </tr>
-    </tfoot>
-  </table>
-  <table>
-    <thead>
-      <tr>
-        <th colspan="2">Balance at a Specified Year</th>
-      </tr>
-    </thead>
-    <tbody></tbody>
-    <tfoot>
-      <tr>
-        <th>Monthly Payment</th>
-        <th>85.68</th>
-      </tr>
-    </tfoot>
-  </table>
+
+  <!-- left:begin -->
+  <div>
+
+    <!-- table-left-1:begin   -->
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">Mortgage Information</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Loan Amount</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Annual Interest Rate</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Term Length (in Years)</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>First Payment Date</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Compound Period</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Payment Frequency</td>
+          <td>150,000.00</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Monthly Payment</th>
+          <th>85.68</th>
+        </tr>
+      </tfoot>
+    </table>
+    <!-- table-left-1:end -->
+
+    <!-- table-left-2:begin -->
+    <table>
+      <tbody>
+        <tr>
+          <td>Home Value or Price</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Yearly Property Taxes</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Yearly H.O. Insurance</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Monthly PMI</td>
+          <td>150,000.00</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>PITI Payment</th>
+          <th>150,000.00</th>
+        </tr>
+      </tfoot>
+    </table>
+    <!-- table-left-2:end -->
+
+    <!-- table-left-3:begin   -->
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">Extra Payments</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Start at Payment No</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Extra Payment</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Payment Interval</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Extra Annual Payment</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Payment # (1-12)</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Total Extra Payments</td>
+          <td class="output">150,000.00</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Interest Savings</th>
+          <th>85.68</th>
+        </tr>
+      </tfoot>
+    </table>
+    <!-- table-left-3:end -->
+
+    <!-- graph-left:begin -->
+    <div>
+      Balance Over Time
+    </div>
+    <!-- graph-left:end -->
+
+  </div>
+  <!-- left:end -->
+
+  <!-- right:begin -->
+  <div>
+
+    <!-- table-right-1:begin -->
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">Balance at a Specified Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Loan Amount</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Annual Interest Rate</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Term Length (in Years)</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>First Payment Date</td>
+          <td class="output">150,000.00</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Monthly Payment</th>
+          <th>85.68</th>
+        </tr>
+      </tfoot>
+    </table>
+    <!-- table-right-1:end -->
+
+    <!-- table-right-2:begin -->
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">Summary</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Years Until Paid Off</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Number of Payments</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Last Payment Date</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Total Payments</td>
+          <td class="output">150,000.00</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Total Interest</th>
+          <th>85.68</th>
+        </tr>
+      </tfoot>
+    </table>
+    <!-- table-right-2:end -->
+
+    <!-- table-right-3:begin   -->
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">Fixed-Rate or ARM</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Variable or Fixed Rate</td>
+          <td>150,000.00</td>
+        </tr>
+        <tr>
+          <td>Years Rate Remains Fixed</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Interest Rate Cap</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Interest Rate Minimum</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Periods Between Adjustments</td>
+          <td class="output">150,000.00</td>
+        </tr>
+        <tr>
+          <td>Estimated Adjustment</td>
+          <td class="output">150,000.00</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Highest Monthly Payment</th>
+          <th>85.68</th>
+        </tr>
+      </tfoot>
+    </table>
+    <!-- table-right-3:end -->
+
+    <!-- graph-right:begin -->
+    <div>
+      Interest Rate History
+    </div>
+    <!-- graph-right:end -->
+
+    <!-- table-right-4:begin -->
+    <table class="tax-deduction">
+      <thead>
+        <tr>
+          <th colspan="2">Tax Deduction</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Tax Bracket</td>
+          <td>25%</td>
+        </tr>
+        <tr>
+          <td>Effective Rate</td>
+          <td class="output">25%</td>
+        </tr>
+        <tr>
+          <td>Tax Returned</td>
+          <td class="output">25%</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- table-right-4:end -->
+
+  </div>
+  <!-- right:end -->
+
 </div>
+
+<h4>Payment Schedule</h4>
+<table>
+  <thead>
+    <tr>
+      <th>No.</th>
+      <th>Payment Date</th>
+      <th>Year</th>
+      <th>Interest Rate</th>
+      <th>Interest Due</th>
+      <th>Payment Due</th>
+      <th>Extra Payments</th>
+      <th>Additional Payments</th>
+      <th>Principal Paid</th>
+      <th>Balance</th>
+      <th>Tax Refunded</th>
+      <th>Cmltv Tax Returned</th>
+    </tr>
+  </thead>
+  <tfoot></tfoot>
+  <tbody></tbody>
+</table>
